ebb0c0284d36c0c2ef9b611de0245764569caefb
S-TJones <jones.shemar.sj@gmail.com>
2023-08-10 23:13:30 -0500

    Added some test pages

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,20 @@ from flask import render_template
 
 app = Flask(__name__)
 
+# The first route "/" should represent your home page and can simply display your Team name, your name and your photo.
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
