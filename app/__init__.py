1d9f8ef88dbbe79832a53ee840acfc8c7df85ec5
S-TJones <jones.shemar.sj@gmail.com>
2023-08-11 00:08:19 -0500

    Updated Products page

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 # The first route "/" should represent your home page and can simply display your Team name, your name and your photo.
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route("/hello/")
+@app.route("/hello/<name>")
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template("hello.html", name=name)
 
 @app.route("/about/")
 def about():
@@ -20,3 +20,36 @@ def about():
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
+
+
+# Products----------------------------------------------
+# Current list of products
+thumb_drive = {"id": 0, "name": "Thumb/Flash Drives", "price": 23.99, "image": "thumb-drives.jpg"}
+water_bottle = {"id": 1, "name": "Water Bottles", "price": 19.89, "image": "water-bottle-mira.jpg"}
+pack_pencils = {"id": 2, "name": "Pack of Pencils", "price": 2.99, "image": "pencil-pack.jpg"}
+small_speakers = {"id": 3, "name": "Small Speakers", "price": 49.99, "image": "small-speaker.jpg"}
+all_products = [thumb_drive, water_bottle, pack_pencils, small_speakers]
+
+@app.route('/product/')
+def product():
+    return render_template('products.html', products=all_products)
+
+@app.route('/product/<int:product_id>')
+def get_product(product_id=None):
+    
+    # Get number of products
+    products_avail = len(all_products)
+
+    # Verify product number received
+    if product_id in range(products_avail):
+        item = None
+        # Find product
+        for product in all_products:
+            if product["id"] == product_id:
+                print("Found")
+                item = product
+                break
+        
+        return render_template('products-single.html',  product=item)
+    else:
+        return render_template('products.html', products=all_products)
